498cf5ed5994442b0fc42ff02a17b2f5f2046ba9
markinscompany-commits <markins.genai@gmail.com>
2026-08-10 05:12:56 -0700

    Обновление превью 10.08.2026

--- _nuxt/D5RMylV3.js
+++
@@ -1,1 +0,0 @@
-import{A as e,Q as t,S as n,T as r,Y as i}from"./rel-EeoX.js";import{t as a}from"#entry";import{t as o}from"./6YKw4BYw.js";var s={},c={class:`min-h-screen flex flex-col`},l={class:`flex-1`};function u(a,s){let u=o;return i(),r(`div`,c,[n(`main`,l,[t(a.$slots,`default`)]),e(u)])}var d=a(s,[[`render`,u]]);export{d as default};
